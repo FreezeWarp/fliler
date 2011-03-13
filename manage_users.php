8cf36f554696444c1c103204396e77dec709a8a2
joseph <rehtaew@gmail.com>
2011-03-13 17:02:07 -0500

    Various MySQL changes in manage users.

--- manage_users.php
+++ manage_users.php
@@ -16,11 +16,8 @@ echo documentStart('Manage Users');
 
 /* Document Content */
 if ($perm['MngUsrs']) {
-  if (!$sqlConnect = @mysql_connect($mysqlHost,$mysqlUser,$mysqlPassword)) {
+  if (!mysqlConnect($mysqlHost,$mysqlUser,$mysqlPassword,$mysqlDatabase)) {
     echo container('Cannot Connect to MySQL','The MySQL database could not be connected to.',1);
-  }
-  elseif (!$sqlConnect = @mysql_select_db($mysqlDatabase)) {
-    echo container('Cannot Connect to Database','The MySQL database "' . $mysqlDatabase. '" cannot be connected to. Please make sure it exists.',1);
   }
   else {
     switch($_GET['stage']) {
@@ -28,21 +25,22 @@ if ($perm['MngUsrs']) {
       case 1:
         echo "<table border='1'>";
         echo '<tr><th>Username</th><th>Access Directory</th><th>Level</th><th>Password</th><th></th></tr>';
-        $totalUsers = mysql_num_rows(mysql_query('SELECT * FROM `' . $mysqlPrefix . 'users`'));
-        $query = mysql_query('SELECT `username`,`accessDirectory`,`accessLevel`,`password` FROM `' . $mysqlPrefix . 'users`');
-        for ($count = 0; $count < $totalUsers; $count += 1) {
-          $cells = mysql_fetch_assoc($query);
-          echo '<tr><td><a href="javascript:var answer = prompt(\'Choose a New Username:\',\'' . $cells['username'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changename&user=' . $cells['username'] . '&newValue=\' + answer; }">' . (($cells['username']) ? $cells['username'] : '<i>null</i>') . '</a></td><td><a href="javascript:var answer = prompt(\'Choose an Access Directory:\',\'' . $cells['accessDirectory'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changeadir&user=$userID&newValue=\' + answer; }">' . (($cells['accessDirectory']) ? $cells['accessDirectory'] : '<i>Home</i>') . '</a></td></td><td><a href="javascript:var answer = prompt(\'What should the new access level be?\',\'' . $cells['accessLevel'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changealevel&user=' . $cells['username'] . '&newValue=\' + answer; }">' . (($cells['accessLevel']) ? $cells['accessLevel'] : '<i>null</i>') . '</a></td><td><a href="javascript:var answer = prompt(\'Choose a new password:\',\'\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changepassword&user=' . $cells['username'] . '&newValue=\' + answer; }">Change Password</a></td><td><a href="javascript:var answer = confirm(\'Are you sure you want to delete this user?\'); if(answer) { location.href=\'manage_users.php?stage=2&action=delete&user=\'' . $cells['username'] . '\'; }"><img src="images/delete.png" /></a></td></tr>';
+        $totalUsers = sqlArr('SELECT COUNT(*) AS count FROM `' . $mysqlPrefix . 'users`');
+        $users = sqlArr('SELECT `username`,`accessDirectory`,`accessLevel`,`password` FROM `' . $mysqlPrefix . 'users`','username');
+
+        foreach ($users AS $username => $user) {
+          echo '<tr><td><a href="javascript:var answer = prompt(\'Choose a New Username:\',\'' . $user['username'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changename&user=' . $user['username'] . '&newValue=\' + answer; }">' . (($user['username']) ? $user['username'] : '<i>null</i>') . '</a></td><td><a href="javascript:var answer = prompt(\'Choose an Access Directory:\',\'' . $user['accessDirectory'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changeadir&user=$userID&newValue=\' + answer; }">' . (($user['accessDirectory']) ? $user['accessDirectory'] : '<i>Home</i>') . '</a></td></td><td><a href="javascript:var answer = prompt(\'What should the new access level be?\',\'' . $user['accessLevel'] . '\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changealevel&user=' . $user['username'] . '&newValue=\' + answer; }">' . (($user['accessLevel']) ? $user['accessLevel'] : '<i>null</i>') . '</a></td><td><a href="javascript:var answer = prompt(\'Choose a new password:\',\'\'); if(answer) { location.href=\'manage_users.php?stage=2&action=changepassword&user=' . $user['username'] . '&newValue=\' + answer; }">Change Password</a></td><td><a href="javascript:var answer = confirm(\'Are you sure you want to delete this user?\'); if(answer) { location.href=\'manage_users.php?stage=2&action=delete&user=\'' . $user['username'] . '\'; }"><img src="images/delete.png" /></a></td></tr>';
         }
         echo '</table><br /><a href="manage_users.php?stage=2&action=new"><img style="padding-right: 20px;" src="images/add-user.png" />Add a New User</a> | <a href="index.php">Return to Index</a>';
       break;
+
       case 2:
-      $user = $_GET['user'];
-      $newValue = $_GET['newValue'];
+      $user = mysqlEscape($_GET['user']);
+      $newValue = mysqlEscape($_GET['newValue']);
       switch($_GET['action']) {
         case 'changename':
           echo '<b>Change Username</b><br />';
-          if (mysql_query('UPDATE `' . $mysqlPrefix . 'users` SET `username` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
+          if (mysqlQuery('UPDATE `' . $mysqlPrefix . 'users` SET `username` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
             echo $user . ' was updated successfully.<br /><br /><a href="manage_users.php">Click here</a> to return to managing users.';
           }
           else {
@@ -50,7 +48,7 @@ if ($perm['MngUsrs']) {
           }
         break;
         case 'changealevel':
-          if (mysql_query('UPDATE `' . $mysqlPrefix . 'users` SET `accessLevel` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
+          if (mysqlQuery('UPDATE `' . $mysqlPrefix . 'users` SET `accessLevel` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
             echo $user . ' was updated successfully.<br /><br /><a href="manage_users.php">Click here</a> to return to managing users.';
           }
           else {
@@ -58,7 +56,7 @@ if ($perm['MngUsrs']) {
           }
         break;
         case 'changeadir':
-          if (mysql_query('UPDATE `' . $mysqlPrefix . 'users` SET `accessDirectory` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
+          if (mysqlQuery('UPDATE `' . $mysqlPrefix . 'users` SET `accessDirectory` = "' . $newValue . '" WHERE `username` = "' . $user . '"')) {
             echo $user . ' was updated successfully.<br /><br /><a href="manage_users.php">Click here</a> to return to managing users.';
           }
           else {
@@ -67,17 +65,14 @@ if ($perm['MngUsrs']) {
         break;
         case 'new':
           // Generate possible access levels.
-          $levels = mysql_query('SELECT `id` FROM `' . $mysqlPrefix . 'levels`');
-          while (false !== ($row = mysql_fetch_row($levels))) {
-            $levelText .= '<option>' . $row[0] . '</option>';
-          }
+          $levelText = mysqlReadThrough(mysqlQuery('SELECT `id` FROM `' . $mysqlPrefix . 'levels`'),'<option>$id</option>');
           echo '<form action="manage_users.php?stage=2&action=new2" method="post"><table><tr><td>Username</td><td><input type="text" name="newusername" /></td></tr><tr><td>Password</td><td><input type="password" name="newpassword" /></td></tr><tr><td>Access Directory (Optional)</td><td><input type="text" name="newaccessDirectory" /></td></tr><tr><td>Access Level</td><td><select name="newaccessLevel">' . $levelText . '</select></td></tr></table><br /><br /><input type="submit" value="Add" /></form>';
         break;
         case 'new2':
           echo '<b>Add a New User</b><br />';
           // Generate a random string.
           $salt = randomString(5);
-          if (mysql_query('INSERT INTO `' . $mysqlPrefix . 'users`
+          if (mysqlQuery('INSERT INTO `' . $mysqlPrefix . 'users`
 			   SET `username` = "' . mysqlEscape($_POST['newusername']) . '",
 			       `salt` = "' . $salt . '",
 			       `password` = "' . md5(md5($_POST['newnpassword']) . $salt) . '",
