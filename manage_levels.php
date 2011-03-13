f0a575c58333f6d54a6cc02561e3d7f4b203b716
joseph <rehtaew@gmail.com>
2011-03-13 17:05:16 -0500

    Various mysql changes for level manager.

--- manage_levels.php
+++ manage_levels.php
@@ -16,23 +16,20 @@ echo documentStart('Manage Levels');
 
 /* Document Content */
 if ($perm['MngLvls']) {
-  if (!mysql_connect($mysqlHost,$mysqlUser,$mysqlPassword)) {
-    trigger_error('MySQL could not be conncted to.',E_USER_ERROR);
-  }
-  elseif (!mysql_select_db($mysqlDatabase)) {
-    trigger_error('The MySQL database "' . $mysqlDatabase . '" could not be accessed.',E_USER_ERROR);
+  if (!mysqlConnect($mysqlHost,$mysqlUser,$mysqlPassword,$mysqlDatabase)) {
+    echo container('Cannot Connect to MySQL','The MySQL database could not be connected to.',1);
   }
   else {
     switch($_GET['do']) {
       // Show Levels
       case null:
       case 'view':
-      $totalLevels = mysql_num_rows(mysql_query('SELECT * FROM `' . $mysqlPrefix . 'levels`'));
+      $totalLevels = sqlArr('SELECT COUNT(*) AS count FROM `' . $mysqlPrefix . 'levels`');
+
       echo '<b>View Levels</b><br /><table class="generic"><tr><th>Level</th><th>Create Files</th><th>Move Files</th><th>Edit Files</th><th>Chmod Files</th><th>Delete Files</th><th>Create Dirs</th><th>Move Dirs</th><th>Chmod Dirs</th><th>Delete Dirs</th><th>View</th><th>Manage Users</th><th>Manage Bckps</th><th>Manage Levels</th></tr>';
-      $query = mysql_query('SELECT * FROM `' . $mysqlPrefix . 'levels');
-      for ($count = 0; $count < $totalLevels; $count += 1) {
-        $cells = mysql_fetch_assoc($query);
-        echo '<tr><td><b>' . $cells['id'] . '</b></td><td><a href="manage_levels.php?do=edit&value=MkF&row=' . $cells['id'] . '">' . boolOutput($cells['MkF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=MvF&row=' . $cells['id'] . '">' . boolOutput($cells['MvF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=EdF&row=' . $cells['id'] . '">' . boolOutput($cells['EdF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=ChmdF&row=' . $cells['id'] . '">' . boolOutput($cells['ChmdF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=RmF&row=' . $cells['id'] . '">' . boolOutput($cells['RmF']) . '</td><td><a href="manage_levels.php?do=edit&value=MkD&row=' . $cells['id'] . '">' . boolOutput($cells['MkD']) . '</td><td><a href="manage_levels.php?do=edit&value=MvD&row=' . $cells['id'] . '">' . boolOutput($cells['MvD']) . '</td><td><a href="manage_levels.php?do=edit&value=ChmdD&row=' . $cells['id'] . '">' . boolOutput($cells['ChmdD']) . '</td><td><a href="manage_levels.php?do=edit&value=RmD&row=' . $cells['id'] . '">' . boolOutput($cells['RmD']) . '</td><td><a href="manage_levels.php?do=edit&value=View&row=' . $cells['id'] . '">' . boolOutput($cells['View']) . '</td><td><a href="manage_levels.php?do=edit&value=MngUsrs&row=' . $cells['id'] . '">' . boolOutput($cells['MngUsrs']) . '</td><td><a href="manage_levels.php?do=edit&value=MngBckps&row=' . $cells['id'] . '">' . boolOutput($cells['MngBckps']) . '</td><td><a href="manage_levels.php?do=edit&value=MngLvls&row=' . $cells['id'] . '">' . boolOutput($cells['MngLvls']) . '</td></tr>';
+      $levels = sqlArr('SELECT * FROM `' . $mysqlPrefix . 'levels','id');
+      foreach ($levels AS $id => $level) {
+        echo '<tr><td><b>' . $level['id'] . '</b></td><td><a href="manage_levels.php?do=edit&value=MkF&row=' . $level['id'] . '">' . boolOutput($level['MkF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=MvF&row=' . $level['id'] . '">' . boolOutput($level['MvF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=EdF&row=' . $level['id'] . '">' . boolOutput($level['EdF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=ChmdF&row=' . $level['id'] . '">' . boolOutput($level['ChmdF']) . '</a></td><td><a href="manage_levels.php?do=edit&value=RmF&row=' . $level['id'] . '">' . boolOutput($level['RmF']) . '</td><td><a href="manage_levels.php?do=edit&value=MkD&row=' . $level['id'] . '">' . boolOutput($level['MkD']) . '</td><td><a href="manage_levels.php?do=edit&value=MvD&row=' . $level['id'] . '">' . boolOutput($level['MvD']) . '</td><td><a href="manage_levels.php?do=edit&value=ChmdD&row=' . $level['id'] . '">' . boolOutput($level['ChmdD']) . '</td><td><a href="manage_levels.php?do=edit&value=RmD&row=' . $level['id'] . '">' . boolOutput($level['RmD']) . '</td><td><a href="manage_levels.php?do=edit&value=View&row=' . $level['id'] . '">' . boolOutput($level['View']) . '</td><td><a href="manage_levels.php?do=edit&value=MngUsrs&row=' . $level['id'] . '">' . boolOutput($level['MngUsrs']) . '</td><td><a href="manage_levels.php?do=edit&value=MngBckps&row=' . $level['id'] . '">' . boolOutput($level['MngBckps']) . '</td><td><a href="manage_levels.php?do=edit&value=MngLvls&row=' . $level['id'] . '">' . boolOutput($level['MngLvls']) . '</td></tr>';
       }
       echo '</table><br /><br /><a href="manage_levels.php?do=add"><img src="images/add.png" style="padding-right: 20px;" />Add New Access Level</a> | <a href="index.php">Return to the Index</a>';
       break;
@@ -42,9 +39,9 @@ if ($perm['MngLvls']) {
       break;
       // Process Editted Value
       case 'edit2':
-      if (mysql_query('UPDATE `' . $mysqlPrefix . 'levels`
-		       SET `' . $_GET['value'] . '` = "' . $_POST['newValue'] . '"
-		       WHERE `id` = "' . $_GET['row'] . '"')) {
+      if (mysqlQuery('UPDATE `' . $mysqlPrefix . 'levels`
+		       SET `' . mysqlEscape($_GET['value']) . '` = "' . mysqlEscape($_POST['newValue']) . '"
+		       WHERE `id` = "' . intval($_GET['row']) . '"')) {
         echo 'The value was updated successfully.<br /><br /><a href="manage_levels.php">Return.</a>';
       }
       break;
@@ -53,7 +50,7 @@ if ($perm['MngLvls']) {
         echo '<form action="manage_levels.php?do=add2" method="post"><table><tr><td>Create Files (MkF): </td><td><select name="MkF"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Move Files (MvF): </td><td><select name="MvF"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Edit Files (EdF): </td><td><select name="EdF"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Change Permissions of Files (ChmdF): </td><td><select name="ChmdF"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Remove Files (RmF): </td><td><select name="RmF"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Create Directories (MkD): </td><td><select name="MkD"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Move Directories (MvD): </td><td><select name="MvD"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Change Permissions of Directories (ChmdD): </td><td><select name="ChmdD"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Remove Directories (RmD): </td><td><select name="RmD"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>View Files and Directories (View): </td><td><select name="View"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Manager Users (MngUsrs): </td><td><select name="MngUsrs"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Manage Backups (MngBckps): </td><td><select name="MngBckps"><option value="0">False</option><option value="1">True</option></select></td></tr><tr><td>Manage Levels (MngLvls): </td><td><select name="MngLvls"><option value="0">False</option><option value="1">True</option></select></td></tr></table><br /><input type="submit" value="Add" /></form>';
       break;
       case 'add2':
-      if (mysql_query('INSERT INTO `' . $mysqlPrefix . 'levels`
+      if (mysqlQuery('INSERT INTO `' . $mysqlPrefix . 'levels`
 		       SET `MkF` = "' . ($_POST['MkF'] ? 1 : 0) . '",
 			   `MvF` = "' . ($_POST['MvF'] ? 1 : 0) . '",
 			   `EdF` = "' . ($_POST['EdF'] ? 1 : 0) . '",
